967176a6356b47691b93277249d4ad471b78e9be
Jay <80186604+jay-fivem@users.noreply.github.com>
2022-02-18 20:32:05 +1030

    Easier Config

--- config.lua
+++ config.lua
@@ -1,3 +1,10 @@
 Config = {}
 
 Config["qb-target"] = false
+
+Config["Masks"] = {
+    ["mask"] = {
+        drawableId = 169,
+        textureId = 13,
+    },
+}
--- server/main.lua
+++ server/main.lua
@@ -2,42 +2,82 @@ local QBCore = exports['qb-core']:GetCoreObject()
 local skin
 local data = {}
 
-QBCore.Functions.CreateUseableItem("mask", function(source, item) -- change this
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    local ped = GetPlayerPed(src)
-    local model = GetEntityModel(ped)
-    if Player.Functions.RemoveItem("mask", 1) then -- change this
-        if model ~= nil then
-            local result = MySQL.Sync.fetchAll('SELECT * FROM playerskins WHERE citizenid = ? AND active = ?', { Player.PlayerData.citizenid, 1 })
-            if result[1] ~= nil then
-                skin = json.decode(result[1].skin)
+
+for k, v in pairs(Config["Masks"]) do
+    QBCore.Functions.CreateUseableItem(k, function(source, item)   
+        local src = source
+        local Player = QBCore.Functions.GetPlayer(src)
+        local ped = GetPlayerPed(src)
+        local model = GetEntityModel(ped)
+        if Player.Functions.RemoveItem("mask", 1) then -- change this
+            if model ~= nil then
+                local result = MySQL.Sync.fetchAll('SELECT * FROM playerskins WHERE citizenid = ? AND active = ?', { Player.PlayerData.citizenid, 1 })
+                if result[1] ~= nil then
+                    skin = json.decode(result[1].skin)
+                end
+    
+                -- MASK BS
+                skin["mask"].item = v.drawableId
+                skin["mask"].texture = v.textureId
+    
+                data[src] = {
+                    mask = v.drawableId,
+                    item = "mask"
+                }
+    
+                SetPedComponentVariation(ped, 1, skin["mask"].item, skin["mask"].texture, 2)
+                TaskPlayAnim(ped, "mp_masks@standard_car@ds@", "put_on_mask", 8.00, -8.00, 800, 51, 0.00, 0, 0, 0)
+                skin = json.encode(skin)
+                -- TODO: Update primary key to be citizenid so this can be an insert on duplicate update query
+                MySQL.Async.execute('DELETE FROM playerskins WHERE citizenid = ?', { Player.PlayerData.citizenid }, function()
+                    MySQL.Async.insert('INSERT INTO playerskins (citizenid, model, skin, active) VALUES (?, ?, ?, ?)', {
+                        Player.PlayerData.citizenid,
+                        model,
+                        skin,
+                        1
+                    })
+                end)
             end
+        end 
+    end)
+end
 
-            -- MASK BS
-            skin["mask"].item = 55
-            skin["mask"].texture = 0
+-- QBCore.Functions.CreateUseableItem("mask", function(source, item) -- change this
+--     local src = source
+--     local Player = QBCore.Functions.GetPlayer(src)
+--     local ped = GetPlayerPed(src)
+--     local model = GetEntityModel(ped)
+--     if Player.Functions.RemoveItem("mask", 1) then -- change this
+--         if model ~= nil then
+--             local result = MySQL.Sync.fetchAll('SELECT * FROM playerskins WHERE citizenid = ? AND active = ?', { Player.PlayerData.citizenid, 1 })
+--             if result[1] ~= nil then
+--                 skin = json.decode(result[1].skin)
+--             end
 
-            data[src] = {
-                mask = 55,
-                item = "mask"
-            }
+--             -- MASK BS
+--             skin["mask"].item = 55
+--             skin["mask"].texture = 0
 
-            SetPedComponentVariation(ped, 1, skin["mask"].item, skin["mask"].texture, 2)
-            TaskPlayAnim(ped, "mp_masks@standard_car@ds@", "put_on_mask", 8.00, -8.00, 800, 51, 0.00, 0, 0, 0)
-            skin = json.encode(skin)
-            -- TODO: Update primary key to be citizenid so this can be an insert on duplicate update query
-            MySQL.Async.execute('DELETE FROM playerskins WHERE citizenid = ?', { Player.PlayerData.citizenid }, function()
-                MySQL.Async.insert('INSERT INTO playerskins (citizenid, model, skin, active) VALUES (?, ?, ?, ?)', {
-                    Player.PlayerData.citizenid,
-                    model,
-                    skin,
-                    1
-                })
-            end)
-        end
-    end
-end)
+--             data[src] = {
+--                 mask = 55,
+--                 item = "mask"
+--             }
+
+--             SetPedComponentVariation(ped, 1, skin["mask"].item, skin["mask"].texture, 2)
+--             TaskPlayAnim(ped, "mp_masks@standard_car@ds@", "put_on_mask", 8.00, -8.00, 800, 51, 0.00, 0, 0, 0)
+--             skin = json.encode(skin)
+--             -- TODO: Update primary key to be citizenid so this can be an insert on duplicate update query
+--             MySQL.Async.execute('DELETE FROM playerskins WHERE citizenid = ?', { Player.PlayerData.citizenid }, function()
+--                 MySQL.Async.insert('INSERT INTO playerskins (citizenid, model, skin, active) VALUES (?, ?, ?, ?)', {
+--                     Player.PlayerData.citizenid,
+--                     model,
+--                     skin,
+--                     1
+--                 })
+--             end)
+--         end
+--     end
+-- end)
 
 RegisterCommand("maskoff", function(source)
     local src = source
